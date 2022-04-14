aacb96c1bf924ee3330c7844b3e575d4443e5bf2
Jay <95605860+jaykorea@users.noreply.github.com>
2022-04-14 15:29:59 +0900

    Update mpu6050.c

--- Core/Src/mpu6050.c
+++ Core/Src/mpu6050.c
@@ -1,35 +1,3 @@
-/*
- * mpu6050.c
- *
- *  Created on: Nov 13, 2019
- *      Author: Bulanov Konstantin
- *
- *  Contact information
- *  -------------------
- *
- * e-mail   :  leech001@gmail.com
- */
-
-/*
- * |---------------------------------------------------------------------------------
- * | Copyright (C) Bulanov Konstantin,2021
- * |
- * | This program is free software: you can redistribute it and/or modify
- * | it under the terms of the GNU General Public License as published by
- * | the Free Software Foundation, either version 3 of the License, or
- * | any later version.
- * |
- * | This program is distributed in the hope that it will be useful,
- * | but WITHOUT ANY WARRANTY; without even the implied warranty of
- * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * | GNU General Public License for more details.
- * |
- * | You should have received a copy of the GNU General Public License
- * | along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * |
- * | Kalman filter algorithm used from https://github.com/TKJElectronics/KalmanFilter
- * |---------------------------------------------------------------------------------
- */
 
 #include <math.h>
 #include "mpu6050.h"
